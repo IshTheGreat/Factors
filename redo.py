ae9ebd565366fb30dd4abb30c2ea23dedeaf1658
IshTheGreat <65795098+IshTheGreat@users.noreply.github.com>
2020-07-20 18:17:38 -0700

    Update redo.py

--- redo.py
+++ redo.py
@@ -6,3 +6,11 @@ for i in range(1, num + 1):
 print("The factors of " + str(num) + " is " + str(thislist)
 for x in thislist:
   print(x)
+if len(thislist) == 2:
+      print("Your number is prime!")
+elif len(thislist) > 2:
+      print("Your number is composite!)
+x = len(thislist)
+print(str(num) + " has " + str(x) + " factors."
+         
+ 
